5086e45200f5804527b0634fc31e71eeff0984be
lulemba-243 <lnitumosi@gmail.com>
2021-04-18 01:41:50 +0300

    button actions

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,13 +3,90 @@ package com.example.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText textBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textBox = findViewById(R.id.display);
     }
 
+    public void zeroButton(View view){
+
+    }
+
+    public void decimalButton(View view){
+
+    }
+
+    public void equalButton(View view){
+
+    }
+
+    public void multiplyButton(View view){
+
+    }
+
+    public void threeButton(View view){
+
+    }
+
+    public void twoButton(View view){
+
+    }
+
+    public void oneButton(View view){
+
+    }
+
+    public void divideButton(View view){
+
+    }
+
+    public void sixButton(View view){
+
+    }
+
+    public void fiveButton(View view){
+
+    }
+
+    public void fourButton(View view){
+
+    }
+
+    public void subtractButton(View view){
+
+    }
+
+    public void nineButton(View view){
+
+    }
+
+    public void eightButton(View view){
+
+    }
+
+    public void sevenButton(View view){
+
+    }
+
+    public void addButton(View view){
+
+    }
+
+    public void deleteButton(View view){
+
+    }
+
+    public void clearButton(View view){
+
+    }
 }
